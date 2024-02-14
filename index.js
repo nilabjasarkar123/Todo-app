d148d3763e8841e1f32e39a17641b1a98cc4a502
nilabja1 <nilabja.work@gmail.com>
2024-02-14 14:53:31 +0530

    added BE

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require('express')
+const express = require('express');
+const { createTodo, updateTodo } = require('./types');
 const app = express()
 const port = 3000
 
 app.use(express.json());
 
 app.post('/todo', (req, res) => {
-    res.send("Hello world")
+    const createPayload = req.body;
+    const parsedPaylaod = createTodo.safeParse(createPayload);
+    if(!parsedPaylaod.success){
+        res.status(411).json({
+            msg : "You send wrong inputs",
+        })
+        return;
+    }
+    // put it in mongodb
+
 })
 
 app.get('/todos', (req, res) => {
@@ -13,8 +23,16 @@ app.get('/todos', (req, res) => {
 })
 
 app.put('/completed', (req, res) => {
-    res.send("Hello world")
+    const updatePayload = req.body;
+    const parsedPaylaod = updateTodo.safeParse(updatePayload);
+    if(!parsedPaylaod.success){
+        res.status(411).json({
+            msg : "You already completed",
+        })
+        return;
+    }
+    // put it in mongodb
 })
 app.listen(port, ()=>{
-    console.log(`app listning on port ${port}`)
+    
 })
