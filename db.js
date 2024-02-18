2b6ff0a4fac90ab0b30f426b5b49ea84492f4b1e
nilabjasarkar123 <nil.sarkar002@gmail.com>
2024-02-18 15:30:21 +0530

    db localhost

--- db.js
+++ db.js
@@ -1,18 +1,17 @@
-import mongoose from 'mongoose';
-import { boolean } from 'zod';
+import mongoose from "mongoose";
+import { boolean } from "zod";
 const { Schema } = mongoose;
 
 const mongoose = require("mongoose");
 
-
-mongoose.connect("")
+mongoose.connect("mongodb://127.0.0.1/todo-db");
 const todoSchema = mongoose.Schema({
-    title: string,
-    description: string,
-    completed: boolean
-})
+  title: string,
+  description: string,
+  completed: boolean,
+});
 const todo = mongoose.model("todos", todoSchema);
 module.exports = {
-    //  todo: todo
-    todo
-}
+  //  todo: todo
+  todo,
+};
--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-const express = require('express');
-const { createTodo, updateTodo } = require('./types');
-const app = express()
-const port = 3000
+const express = require("express");
+const { createTodo, updateTodo } = require("./types");
+const app = express();
+const { todo } = require("todo-db");
+const port = 3000;
 
 app.use(express.json());
 
-app.post('/todo',async, (req, res) => {
-    const createPayload = req.body;
-    const parsedPaylaod = createTodo.safeParse(createPayload);
-    if(!parsedPaylaod.success){
-        res.status(411).json({
-            msg : "You send wrong inputs",
-        })
-        return;
+app.post("/todo", async (req, res) => {
+  const createPayload = req.body;
+  const parsedPaylaod = createTodo.safeParse(createPayload);
+  if (!parsedPaylaod.success) {
+    res.status(411).json({
+      msg: "You send wrong inputs",
+    });
+    return;
+  }
+  // put it in mongodb
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completd: false,
+  });
+  res.json({  
+    msg: "Todo created",
+  });
+});
+
+app.get("/todos", async (req, res) => {
+  const todos = await todo.find({});
+  res.json({
+    todos,
+  });
+});
+
+app.put("/completed", async (req, res) => {
+  const updatePayload = req.body;
+  const parsedPaylaod = updateTodo.safeParse(updatePayload);
+  if (!parsedPaylaod.success) {
+    res.status(411).json({
+      msg: "You send wrong inputs",
+    });
+    return;
+  }
+  // put it in mongodb
+  await todo.update(
+    {
+      _id: req.body.id,
+    },
+    {
+      completd: true,
     }
-    // put it in mongodb
-    await.todo.create({
-        title: createPayload.title,
-        description: createPayload.description,
-        completd: false,
-    })
-    res.json({
-        msg: "Todo created"
-    })
-})
+  );
 
-app.get('/todos', async, (req, res) => {
-    const todos = await.todo.find({})
-    res.json({
-        todos
-    })
-})
-
-app.put('/completed', async, (req, res) => {
-    const updatePayload = req.body;
-    const parsedPaylaod = updateTodo.safeParse(updatePayload);
-    if(!parsedPaylaod.success){
-        res.status(411).json({
-            msg : "You send wrong inputs",
-        })
-        return;
-    }
-    // put it in mongodb
-    await.todo.update({
-        _id: req.body.id
-        }, {
-            completd: true,
-    })
-    
-    res.json({
-        msg: "Todo marked completed"
-    })
-
-    
-})
-app.listen(port, ()=>{
-    
-})
+  res.json({
+    msg: "Todo marked completed",
+  });
+});
+app.listen(port, () => {});
